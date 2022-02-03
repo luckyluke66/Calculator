e1d2e6d0128312164ff059fbeb80c87e240d4a90
luckyluke66 <lukasbalog66@gmail.com>
2022-02-03 19:02:32 +0100

    znamenka, cisla z tlacitek

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -3,11 +3,14 @@ package com.example.calculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     private var tvInput: TextView? = null
+
+    var lastNumeric : Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,10 +21,33 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun onDigit(view: View) {
-        tvInput?.append("1")
+        tvInput?.append((view as Button).text)
+        lastNumeric = true
     }
 
-    fun test(){
+    fun onClear(view: View){
+        tvInput?.text = ""
+    }
 
+    fun onOperator(view: View){
+        tvInput?.text?.let{
+            if(lastNumeric && !isOperatorAdded(it.toString())){
+                tvInput?.append((view as Button).text)
+                lastNumeric = false
+            }
+        }
+    }
+
+
+
+    private fun isOperatorAdded(value: String) : Boolean {
+        return if(value.startsWith("-")){
+            false
+        }else{
+            value.contains("/")
+                    || value.contains("*")
+                    || value.contains("+")
+                    || value.contains("-")
+        }
     }
 }
